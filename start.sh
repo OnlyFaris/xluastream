de6de01cd1e9d5de296d8ad5ac14f1fcdb706dba
OnlyFaris <60811972+OnlyFaris@users.noreply.github.com>
2021-10-11 18:23:40 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,5 +3,7 @@ git clone -b alpha https://github.com/OnlyFaris/xluastream.git /xluastream
 cd /xluastream
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
+pip install -U youtube-dl
+youtube-dl -U
 echo "Starting Bot, Please Wait..."
 python3 main.py
